b80d7706c83b448a2a04343474f0fd6addf84528
Morteza Raeisi <mrraeisi@outlook.com>
2023-12-24 21:23:31 +0330

    [: init SSR app :]

---
+++ rustify-ssr/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="grid gap-3">
+      <div class="col-6">1</div>
+      <div class="columns-6">2</div>
+    </div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
